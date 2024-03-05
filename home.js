4753de723c73fc360e183d17a9f51dba01ac1899
FlorianBrunel <florian.brunel@viacesi.fr>
2024-03-05 11:44:43 +0100

    Fin avant responsive

---
+++ home.js
@@ -0,0 +1,15 @@
+function changecoeur() {
+    document.getElementById('coeur').src = 'Images/coeur-bleu.png'; 
+}
+
+function resetcoeur() {
+    document.getElementById('coeur').src = 'Images/coeur.png'; 
+}
+
+function changeloupe() {
+    document.getElementById('loupe').src = 'Images/loupe-bleu.png'; 
+}
+
+function resetloupe() {
+    document.getElementById('loupe').src = 'Images/loupe.png'; 
+}
