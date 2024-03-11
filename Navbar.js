4b2973ee56f0b86b2a4fc1e51e54e23c109860c4
Tom5115 <laine.tom4@gmail.com>
2024-03-11 10:39:22 +0100

    navbar

---
+++ Navbar.js
@@ -0,0 +1,48 @@
+function changecoeur() {
+    document.getElementById('coeur').src = 'Images/coeur-bleu.png'; 
+}
+
+function resetcoeur() {
+    document.getElementById('coeur').src = 'Images/coeur.png'; 
+}
+
+function changeloupe() {
+    document.getElementById('loupe').src = 'Images/loupe-bleu.png'; 
+}
+
+function resetloupe() {
+    document.getElementById('loupe').src = 'Images/loupe.png'; 
+}
+
+function changeloupe() {
+    document.getElementById('loupeGR').src = 'Images/loupe-bleu.png'; 
+}
+
+function resetloupe() {
+    document.getElementById('loupeGR').src = 'Images/loupe.png'; 
+}
+
+function changemenu() {
+    document.getElementById('menuGR').src = 'Images/slider-bleu.png'; 
+}
+
+function resetmenu() {
+    document.getElementById('menuGR').src = 'Images/slider.png'; 
+}
+
+
+
+/*----------------Afficher navbar------------------------------------------------*/
+
+
+function toggleSearchBox() {
+    var rechercheGR = document.getElementById('rechercheGR');
+    rechercheGR.classList.toggle('affiche');
+}
+
+function toggleMenu() {
+    var menu = document.getElementById("menu");
+    menu.classList.toggle("show");
+}
+
+
