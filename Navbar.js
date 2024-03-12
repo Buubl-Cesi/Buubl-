1dfb72cd2b83a6deaa5c771bba88b70927590843
Tom5115 <laine.tom4@gmail.com>
2024-03-12 16:14:23 +0100

    modif navbar caché

--- Navbar.js
+++ Navbar.js
@@ -24,9 +24,51 @@ function resetmenu() {
 
 /*----------------Afficher navbar------------------------------------------------*/
 
+let pagetop = true;
+let menus = false;
+
+window.addEventListener('scroll', function() {
+    const navbar = document.getElementById('navbar');
+    const threshold = 10; // Vous pouvez ajuster ce seuil si nécessaire
+    const currentScroll = window.scrollY;
+
+    // Vérifie si la page est en haut
+    if (currentScroll <= threshold) {
+        navbar.classList.remove('navbar-hidden');
+        pagetop = true;
+    } 
+    else {
+        navbar.classList.add('navbar-hidden');
+        pagetop = false;
+        menus = false;
+        menu.classList.remove('show');    
+    }
+});
+
+window.addEventListener('mousemove', function(e) {
+    const cursorPositionY = e.clientY
+    const navbar = document.getElementById('navbar');
+    const threshold = 80;
+
+    if (cursorPositionY <= threshold && pagetop == false) {
+        navbar.classList.remove('navbar-hidden');
+    } 
+    else if (cursorPositionY > threshold && pagetop == false && menus == false) {
+        navbar.classList.add('navbar-hidden');
+    }
+});
+
+
+
 function toggleMenu() {
     var menu = document.getElementById("menu");
     menu.classList.toggle("show");
+    if (menus == false){
+        menus = true;
+    }
+    else{
+        menus = false;
+    }
 }
 
 document.addEventListener('click', function(e) {
@@ -34,8 +76,10 @@ document.addEventListener('click', function(e) {
     var menuToggle = document.getElementById("menuGR"); 
     var target = e.target; 
     if (!menu.contains(target) && !menuToggle.contains(target)) {
+        menus = false;
         if (menu.classList.contains('show')) {
             menu.classList.remove('show');
+            
         }
     }
 });
