a9ee778e8118b536ae3562926787b1717c762069
Tom5115 <laine.tom4@gmail.com>
2024-03-12 10:33:48 +0100

    ajout navbar

--- home.js
+++ home.js
@@ -13,3 +13,48 @@ function changeloupe() {
 function resetloupe() {
     document.getElementById('loupe').src = 'Images/loupe.png'; 
 }
+
+function changemenu() {
+    document.getElementById('menuGR').src = 'Images/slider-bleu.png'; 
+}
+
+function resetmenu() {
+    document.getElementById('menuGR').src = 'Images/slider.png'; 
+}
+
+/*----------------Afficher navbar------------------------------------------------*/
+
+function toggleMenu() {
+    var menu = document.getElementById("menu");
+    menu.classList.toggle("show");
+}
+
+document.addEventListener('click', function(e) {
+    var menu = document.getElementById("menu");
+    var menuToggle = document.getElementById("menuGR"); 
+    var target = e.target; 
+    if (!menu.contains(target) && !menuToggle.contains(target)) {
+        if (menu.classList.contains('show')) {
+            menu.classList.remove('show');
+        }
+    }
+});
+
+function updateImageSource() {
+    var width = window.innerWidth;
+    var image = document.getElementById('logo');
+  
+    if (width < 1400 && width > 1100) {
+      image.src = 'Images/logo.png';
+    } 
+    else if (width < 1100 && width > 400) {
+      image.src = 'Images/mini-logo.png';
+    }
+    else if (width < 400){
+      image.src = 'Images/mini-logo.png';
+    }
+    
+    
+  }
+  window.addEventListener('resize', updateImageSource);
+  updateImageSource();
