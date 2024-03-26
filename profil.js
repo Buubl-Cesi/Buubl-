ddfcc7a8bc7214fceeb366f31154eaac6e8ada30
Tom5115 <laine.tom4@gmail.com>
2024-03-26 11:12:24 +0100

    change profil

--- profil.js
+++ profil.js
@@ -17,14 +17,11 @@ document.getElementById('selecteurFichier').addEventListener('change', function(
                 
                 var ctx = canvas.getContext('2d');
                 
-                // Calcule le point de départ pour le redécoupage
                 var startX = (img.width - taille) / 2;
                 var startY = (img.height - taille) / 2;
                 
-                // Dessine l'image redécoupée sur le canvas
                 ctx.drawImage(img, startX, startY, taille, taille, 0, 0, taille, taille);
                 
-                // Met à jour l'image sur la page avec la nouvelle image redécoupée
                 document.getElementById('imageCliquee').src = canvas.toDataURL();
             };
             
@@ -39,9 +36,9 @@ document.getElementById('selecteurFichier').addEventListener('change', function(
 document.addEventListener("DOMContentLoaded", function() {
     var bouton = document.getElementById("boutonGriser");
     var champs = document.querySelectorAll("input[type='text']");
-    var estActive = false; // Initialisation à false pour indiquer que les champs sont désactivés
+    var estActive = false; 
 
-    // Fonction pour désactiver les champs
+ 
     function desactiverChamps() {
         champs.forEach(function(champ) {
             champ.disabled = true;
@@ -49,13 +46,11 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
-    // Désactiver les champs au chargement de la page
     desactiverChamps();
 
     bouton.addEventListener("click", function() {
-        estActive = !estActive; // Inverse l'état à chaque clic
+        estActive = !estActive; 
 
-        // Active ou désactive les champs
         champs.forEach(function(champ) {
             champ.disabled = !estActive;
             if (!estActive) {
@@ -65,7 +60,6 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         });
 
-        // Change le style et le texte du bouton selon l'état
         if (estActive) {
             bouton.classList.add("bouton-actif");
             bouton.textContent = "Sauvegarder";
@@ -75,110 +69,3 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
-
-
-/*----------------------------------------------------------------------------------------------*/
-
-function changecoeur() {
-    document.getElementById('coeur').src = 'Images/coeur-bleu.png'; 
-}
-
-function resetcoeur() {
-    document.getElementById('coeur').src = 'Images/coeur.png'; 
-}
-
-function changeloupe() {
-    document.getElementById('loupe').src = 'Images/loupe-bleu.png'; 
-}
-
-function resetloupe() {
-    document.getElementById('loupe').src = 'Images/loupe.png'; 
-}
-
-function changemenu() {
-    document.getElementById('menuGR').src = 'Images/slider-bleu.png'; 
-}
-
-function resetmenu() {
-    document.getElementById('menuGR').src = 'Images/slider.png'; 
-}
-
-/*----------------Afficher navbar------------------------------------------------*/
-let pagetop = true;
-let menus = false;
-
-window.addEventListener('scroll', function() {
-    const navbar = document.getElementById('navbar');
-    const threshold = 10; // Vous pouvez ajuster ce seuil si nécessaire
-    const currentScroll = window.scrollY;
-
-    // Vérifie si la page est en haut
-    if (currentScroll <= threshold) {
-        navbar.classList.remove('navbar-hidden');
-        pagetop = true;
-    } 
-    else {
-        navbar.classList.add('navbar-hidden');
-        pagetop = false;
-        menus = false;
-        menu.classList.remove('show');    
-    }
-});
-
-window.addEventListener('mousemove', function(e) {
-    const cursorPositionY = e.clientY
-    const navbar = document.getElementById('navbar');
-    const threshold = 80;
-
-    if (cursorPositionY <= threshold && pagetop == false) {
-        navbar.classList.remove('navbar-hidden');
-    } 
-    else if (cursorPositionY > threshold && pagetop == false && menus == false) {
-        navbar.classList.add('navbar-hidden');
-    }
-});
-
-
-
-function toggleMenu() {
-    var menu = document.getElementById("menu");
-    menu.classList.toggle("show");
-    if (menus == false){
-        menus = true;
-    }
-    else{
-        menus = false;
-    }
-}
-
-document.addEventListener('click', function(e) {
-    var menu = document.getElementById("menu");
-    var menuToggle = document.getElementById("menuGR"); 
-    var target = e.target; 
-    if (!menu.contains(target) && !menuToggle.contains(target)) {
-        menus = false;
-        if (menu.classList.contains('show')) {
-            menu.classList.remove('show');
-            
-        }
-    }
-});
-
-function updateImageSource() {
-    var width = window.innerWidth;
-    var image = document.getElementById('logo');
-  
-    if (width < 1400 && width > 1100) {
-      image.src = 'Images/logo.png';
-    } 
-    else if (width < 1100 && width > 400) {
-      image.src = 'Images/mini-logo.png';
-    }
-    else if (width < 400){
-      image.src = 'Images/mini-logo.png';
-    }
-    
-    
-  }
-  window.addEventListener('resize', updateImageSource);
-  updateImageSource();
