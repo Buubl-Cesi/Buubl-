bea9a0a73d18ec7eec6d69d92b8de99546b05cf1
Tom5115 <laine.tom4@gmail.com>
2024-03-31 18:11:31 +0200

    bdd company

--- StatHub/Stat/StatistiquesEntreprises/models/CompanyModel.php
+++ StatHub/Stat/StatistiquesEntreprises/models/CompanyModel.php
@@ -7,45 +7,153 @@ class CompanyModel {
     }
 
     public function getSector() {	
-        $stmt = $this->pdo->prepare("SELECT DISTINCT `COMPANY_ACTIVITY` FROM `COMPANY`;");
+        $stmt = $this->pdo->prepare("SELECT DISTINCT C.COMPANY_ACTIVITY FROM COMPANY C;");
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
     public function getAllStat() {
         $stmt = $this->pdo->prepare("SELECT 
-                u.STUDENT_NAME AS Nom,
-                u.STUDENT_FNAME AS Prenom,
-                p.PROMOTION_NAME AS Promotion,
-                s.STUDENTS_CMPT AS Compteur
-            FROM
-                STUDENTS s
-            JOIN
-                _USER u ON s.ID_USER = u.ID_USER
-            LEFT JOIN
-                PROMOTION p ON s.ID_PROMOTION = p.ID_PROMOTION;");
+        C.COMPANY_NAME AS nom,
+        C.COMPANY_ACTIVITY AS activite, 
+        CT.CITY_NAME AS ville,
+        C.COMPANY_MARK AS note, 
+        COUNT(AP.ID_APPLICATION) AS NombreApplications
+        FROM COMPANY C
+        JOIN ADDRESS A ON C.ID_ADDRESS = A.ID_ADDRESS
+        JOIN CITY CT ON A.ID_CITY = CT.ID_CITY
+        JOIN INTERNSHIP I ON C.ID_COMPANY = I.ID_COMPANY
+        JOIN APPLICATION AP ON I.ID_INTERNSHIP = AP.ID_INTERNSHIP
+        GROUP BY C.ID_COMPANY;");
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
     public function getAllStatWithParam($orderBy, $orderByCrease, $parameter, $sector) {
-        $stmt = $this->pdo->prepare("SELECT 
-        u.STUDENT_NAME AS Nom,
-        u.STUDENT_FNAME AS Prenom,
-        p.PROMOTION_NAME AS Promotion,
-        s.STUDENTS_CMPT AS Compteur
-    FROM
-        STUDENTS s
-    JOIN
-        _USER u ON s.ID_USER = u.ID_USER
-    LEFT JOIN
-        PROMOTION p ON s.ID_PROMOTION = p.ID_PROMOTION
-    WHERE 
-        u.STUDENT_NAME = :parameter
-    ");
-    
-        $stmt->bindParam(':parameter', $parameter);
-        $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        if ($orderBy == "Sector") {
+            if ($sector == "NoOne") {
+                // Appel de la fonction qui trie par secteur sans paramètre
+                $stmt = $this->pdo->prepare("SELECT 
+                C.COMPANY_NAME AS nom,
+                C.COMPANY_ACTIVITY AS activite, 
+                CT.CITY_NAME AS ville,
+                C.COMPANY_MARK AS note, 
+                COUNT(AP.ID_APPLICATION) AS NombreApplications
+                FROM COMPANY C
+                JOIN ADDRESS A ON C.ID_ADDRESS = A.ID_ADDRESS
+                JOIN CITY CT ON A.ID_CITY = CT.ID_CITY
+                JOIN INTERNSHIP I ON C.ID_COMPANY = I.ID_COMPANY
+                JOIN APPLICATION AP ON I.ID_INTERNSHIP = AP.ID_INTERNSHIP
+                GROUP BY C.ID_COMPANY
+                ORDER BY C.COMPANY_ACTIVITY ASC;");
+            } else {
+                // Appel de la fonction qui trie par secteur avec paramètre
+                $stmt = $this->pdo->prepare("SELECT 
+                C.COMPANY_NAME AS nom,
+                C.COMPANY_ACTIVITY AS activite, 
+                CT.CITY_NAME AS ville,
+                C.COMPANY_MARK AS note, 
+                COUNT(AP.ID_APPLICATION) AS NombreApplications
+                FROM COMPANY C
+                JOIN ADDRESS A ON C.ID_ADDRESS = A.ID_ADDRESS
+                JOIN CITY CT ON A.ID_CITY = CT.ID_CITY
+                JOIN INTERNSHIP I ON C.ID_COMPANY = I.ID_COMPANY
+                JOIN APPLICATION AP ON I.ID_INTERNSHIP = AP.ID_INTERNSHIP
+                WHERE C.COMPANY_ACTIVITY = :secteur
+                GROUP BY C.ID_COMPANY;");
+             $stmt->bindParam(':secteur', $sector);
+            }
+        } 
+        else if ($orderBy == "Graduation") {
+            if ($parameter == "") {
+                if ($orderByCrease == "Increasing") {
+                    // Appel de la fonction qui trie par note croissante 
+                    $stmt = $this->pdo->prepare("SELECT 
+                    C.COMPANY_NAME AS nom,
+                    C.COMPANY_ACTIVITY AS activite, 
+                    CT.CITY_NAME AS ville,
+                    C.COMPANY_MARK AS note, 
+                    COUNT(AP.ID_APPLICATION) AS NombreApplications
+                    FROM COMPANY C
+                    JOIN ADDRESS A ON C.ID_ADDRESS = A.ID_ADDRESS
+                    JOIN CITY CT ON A.ID_CITY = CT.ID_CITY
+                    JOIN INTERNSHIP I ON C.ID_COMPANY = I.ID_COMPANY
+                    JOIN APPLICATION AP ON I.ID_INTERNSHIP = AP.ID_INTERNSHIP
+                    GROUP BY C.ID_COMPANY
+                    ORDER BY C.COMPANY_MARK ASC;");
+                    
+                       
+                } else if ($orderByCrease == "Decreasing") {
+                    // Appel de la fonction qui trie par note décroissante 
+                    $stmt = $this->pdo->prepare("SELECT 
+                    C.COMPANY_NAME AS nom,
+                    C.COMPANY_ACTIVITY AS activite, 
+                    CT.CITY_NAME AS ville,
+                    C.COMPANY_MARK AS note, 
+                    COUNT(AP.ID_APPLICATION) AS NombreApplications
+                    FROM COMPANY C
+                    JOIN ADDRESS A ON C.ID_ADDRESS = A.ID_ADDRESS
+                    JOIN CITY CT ON A.ID_CITY = CT.ID_CITY
+                    JOIN INTERNSHIP I ON C.ID_COMPANY = I.ID_COMPANY
+                    JOIN APPLICATION AP ON I.ID_INTERNSHIP = AP.ID_INTERNSHIP
+                    GROUP BY C.ID_COMPANY
+                    ORDER BY C.COMPANY_MARK DESC;");
+                }
+            } else {
+                // Appel de la fonction qui trie par note avec paramètre
+                $stmt = $this->pdo->prepare("SELECT 
+                    C.COMPANY_NAME AS nom,
+                    C.COMPANY_ACTIVITY AS activite, 
+                    CT.CITY_NAME AS ville,
+                    C.COMPANY_MARK AS note, 
+                    COUNT(AP.ID_APPLICATION) AS NombreApplications
+                    FROM COMPANY C
+                    JOIN ADDRESS A ON C.ID_ADDRESS = A.ID_ADDRESS
+                    JOIN CITY CT ON A.ID_CITY = CT.ID_CITY
+                    JOIN INTERNSHIP I ON C.ID_COMPANY = I.ID_COMPANY
+                    JOIN APPLICATION AP ON I.ID_INTERNSHIP = AP.ID_INTERNSHIP
+                    WHERE C.COMPANY_MARK = :parameter
+                    GROUP BY C.ID_COMPANY;");
+                $stmt->bindParam(':parameter', $parameter);
+            }
+        } 
+        else if ($orderBy == "NumberOffer") {
+            if ($orderByCrease == "Increasing") {
+                // Appel de la fonction qui trie par nombre offre croissante 
+                $stmt = $this->pdo->prepare("SELECT 
+                C.COMPANY_NAME AS nom,
+                C.COMPANY_ACTIVITY AS activite, 
+                CT.CITY_NAME AS ville,
+                C.COMPANY_MARK AS note, 
+                COUNT(AP.ID_APPLICATION) AS NombreApplications
+                FROM COMPANY C
+                JOIN ADDRESS A ON C.ID_ADDRESS = A.ID_ADDRESS
+                JOIN CITY CT ON A.ID_CITY = CT.ID_CITY
+                JOIN INTERNSHIP I ON C.ID_COMPANY = I.ID_COMPANY
+                JOIN APPLICATION AP ON I.ID_INTERNSHIP = AP.ID_INTERNSHIP
+                GROUP BY C.ID_COMPANY
+                ORDER BY NombreApplications ASC;");
+                
+                   
+            } else if ($orderByCrease == "Decreasing") {
+                // Appel de la fonction qui trie par nombre offre décroissante 
+                $stmt = $this->pdo->prepare("SELECT 
+                C.COMPANY_NAME AS nom,
+                C.COMPANY_ACTIVITY AS activite, 
+                CT.CITY_NAME AS ville,
+                C.COMPANY_MARK AS note, 
+                COUNT(AP.ID_APPLICATION) AS NombreApplications
+                FROM COMPANY C
+                JOIN ADDRESS A ON C.ID_ADDRESS = A.ID_ADDRESS
+                JOIN CITY CT ON A.ID_CITY = CT.ID_CITY
+                JOIN INTERNSHIP I ON C.ID_COMPANY = I.ID_COMPANY
+                JOIN APPLICATION AP ON I.ID_INTERNSHIP = AP.ID_INTERNSHIP
+                GROUP BY C.ID_COMPANY
+                ORDER BY NombreApplications DESC;");
+                
+            }
+        }
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 }
--- StatHub/Stat/StatistiquesEntreprises/templates_c/a0da2029fb767d5e8750ef28ed2406ca029ab25c_0.file.stat_company.tpl.php
+++ StatHub/Stat/StatistiquesEntreprises/templates_c/a0da2029fb767d5e8750ef28ed2406ca029ab25c_0.file.stat_company.tpl.php
@@ -1,4 +1,27 @@
-<!DOCTYPE html>
+<?php
+/* Smarty version 3.1.47, created on 2024-03-31 17:36:49
+  from 'C:\Users\laine\Desktop\Serveur\StatHub\Stat\StatistiquesEntreprises\views\templates\stat_company.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.47',
+  'unifunc' => 'content_66098311d46f97_43802124',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'a0da2029fb767d5e8750ef28ed2406ca029ab25c' => 
+    array (
+      0 => 'C:\\Users\\laine\\Desktop\\Serveur\\StatHub\\Stat\\StatistiquesEntreprises\\views\\templates\\stat_company.tpl',
+      1 => 1711899408,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_66098311d46f97_43802124 (Smarty_Internal_Template $_smarty_tpl) {
+?><!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
@@ -68,9 +91,18 @@
                                     <label class="Parameter-label">Secteur :</label>
                                     <select name="sector">
                                         <option value="NoOne">Indiquez un secteur</option>
-                                        {foreach $sector as $s}
-                                            <option value="{$s['COMPANY_ACTIVITY']}">{$s['COMPANY_ACTIVITY']}</option>
-                                        {/foreach}
+                                        <?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['sector']->value, 's');
+$_smarty_tpl->tpl_vars['s']->do_else = true;
+if ($_from !== null) foreach ($_from as $_smarty_tpl->tpl_vars['s']->value) {
+$_smarty_tpl->tpl_vars['s']->do_else = false;
+?>
+                                            <option value="<?php echo $_smarty_tpl->tpl_vars['s']->value['COMPANY_ACTIVITY'];?>
+"><?php echo $_smarty_tpl->tpl_vars['s']->value['COMPANY_ACTIVITY'];?>
+</option>
+                                        <?php
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
                                     </select>
                             </div>
                             <input class = "object-param-button" type="submit" value="Rechercher" />
@@ -90,15 +122,27 @@
                         <th>Nombre d'offres</th>
                         <th>Note</th>
                     </tr>
-                    {foreach $stats as $stat}
+                    <?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['stats']->value, 'stat');
+$_smarty_tpl->tpl_vars['stat']->do_else = true;
+if ($_from !== null) foreach ($_from as $_smarty_tpl->tpl_vars['stat']->value) {
+$_smarty_tpl->tpl_vars['stat']->do_else = false;
+?>
                     <tr>
-                        <td>{$stat.nom}</td>
-                        <td>{$stat.activite}</td>
-                        <td>{$stat.ville}</td>
-                        <td>{$stat.NombreApplications}</td>
-                        <td>{$stat.note}</td>
+                        <td><?php echo $_smarty_tpl->tpl_vars['stat']->value['nom'];?>
+</td>
+                        <td><?php echo $_smarty_tpl->tpl_vars['stat']->value['activite'];?>
+</td>
+                        <td><?php echo $_smarty_tpl->tpl_vars['stat']->value['ville'];?>
+</td>
+                        <td><?php echo $_smarty_tpl->tpl_vars['stat']->value['NombreApplications'];?>
+</td>
+                        <td><?php echo $_smarty_tpl->tpl_vars['stat']->value['note'];?>
+</td>
                     </tr>
-                    {/foreach}
+                    <?php
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
                 </table>
             </div>
         </div>
@@ -107,3 +151,5 @@
 </html>
 
 
+<?php }
+}
